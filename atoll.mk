b6ea95c831e68ac53d2a7f04a73bf01fa16cc8bc
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-23 10:59:48 +0300

    sm6250-common: Build our fingeprint service
    Change-Id: I8bd2a730e3fe4ef6b981094a54a2a608ca9113d7

--- atoll.mk
+++ atoll.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_atoll
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
