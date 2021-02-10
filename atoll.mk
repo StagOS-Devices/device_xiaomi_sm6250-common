8d1643e6cc6d1b5e123844c94cea839375cc2bfa
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:02:15 +0100

    sm6250-common: Build libhwbinder
    * Some libs use it
    
    Change-Id: Ic40f730a4d498725459785b8c33ef7f5e8d0b13b

--- atoll.mk
+++ atoll.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+    libhwbinder.vendor
 
 # Rootdir
 PRODUCT_PACKAGES += \
