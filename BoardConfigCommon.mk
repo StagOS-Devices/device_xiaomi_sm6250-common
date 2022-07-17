c5041d28abf58e87c0ae5d95be41fc3d3396f30c
Edgars Cīrulis <edgarsscirulis@gmail.com>
2022-07-18 00:07:41 +0300

    sm6250-common: Update vendor security patch 2022-04-01
    from joyeuse-user 11 RKQ1.200826.002 V12.5.7.0.RJZMIXM release-keys
    
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -169,7 +169,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2022-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
