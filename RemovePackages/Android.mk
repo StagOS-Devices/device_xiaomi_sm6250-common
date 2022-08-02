9038e369be19778e95113a8e67651885de2c9256
Edgars Cīrulis <edgarsscirulis@gmail.com>
2022-08-02 17:56:13 +0300

    sm6250-common: Add more packages to nuke
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM Videos YouTube YouTubeMusicPrebuilt VzwOmaTrigger Maps Music RecorderPrebuilt SafetyHubPrebuilt PrebuiltGmail Drive
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices Jelly OBDM_Permissions OPScreenRecord OPScreenRecorder SprintDM SprintHM Videos YouTube YouTubeMusicPrebuilt VzwOmaTrigger Maps Music RecorderPrebuilt SafetyHubPrebuilt PrebuiltGmail Drive PulseMusic 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
